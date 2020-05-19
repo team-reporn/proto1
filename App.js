a92c612994150396b703fda804ab85c1e8a7decf
remiruc <remi.rucojevic@gmail.com>
2020-05-19 15:16:57 +0200

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import Shake from './games/shake/shake'
+import Tabou from './games/tabou/tabou'
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -9,9 +11,7 @@ const instructions = Platform.select({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+      <Tabou/>
     </View>
   );
 }
